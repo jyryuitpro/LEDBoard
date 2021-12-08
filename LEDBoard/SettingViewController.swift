9c54e51dc59929527d543fefbfd5a385c7e61317
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-12-08 14:40:17 +0900

    LED 전광판 UI 그리기, 에셋 카탈로그, 기능 구현하기

---
+++ LEDBoard/SettingViewController.swift
@@ -0,0 +1,83 @@
+//
+//  SettingViewController.swift
+//  LEDBoard
+//
+//  Created by 류지영 on 2021/12/08.
+//
+
+import UIKit
+
+protocol LEDBoardSettingDelegate:AnyObject {
+    func changedSetting(text: String?, textColor: UIColor, backgroundColor: UIColor)
+}
+
+class SettingViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var yellowButton: UIButton!
+    @IBOutlet weak var purpleButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var blackButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var orangeButton: UIButton!
+    
+    weak var delegate: LEDBoardSettingDelegate?
+    var ledText: String?
+    var textColor: UIColor = .yellow
+    var backgroundColor: UIColor = .black
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configureView()
+    }
+    
+    private func configureView() {
+        if let ledText = ledText {
+            self.textField.text = ledText
+        }
+        self.changeTextColor(color: self.textColor)
+        self.changeBackgroundColor(color: self.backgroundColor)
+    }
+    
+    @IBAction func tapTextColorButton(_ sender: UIButton) {
+        if sender == self.yellowButton {
+            self.changeTextColor(color: .yellow)
+            self.textColor = .yellow
+        } else if sender == self.purpleButton {
+            self.changeTextColor(color: .purple)
+            self.textColor = .purple
+        } else {
+            self.changeTextColor(color: .green)
+            self.textColor = .green
+        }
+    }
+    
+    @IBAction func tabBackgroundColorButton(_ sender: UIButton) {
+        if sender == self.blackButton {
+            self.changeBackgroundColor(color: .black)
+            self.backgroundColor = .black
+        } else if sender == self.blueButton {
+            self.changeBackgroundColor(color: .blue)
+            self.backgroundColor = .blue
+        } else {
+            self.changeBackgroundColor(color: .orange)
+            self.backgroundColor = .orange
+        }
+    }
+    
+    @IBAction func tabSaveButton(_ sender: UIButton) {
+        self.delegate?.changedSetting(text: self.textField.text, textColor: self.textColor, backgroundColor: self.backgroundColor)
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    private func changeTextColor(color: UIColor) {
+        self.yellowButton.alpha = color == UIColor.yellow ? 1 : 0.2
+        self.purpleButton.alpha = color == UIColor.purple ? 1 : 0.2
+        self.greenButton.alpha = color == UIColor.green ? 1 : 0.2
+    }
+    
+    private func changeBackgroundColor(color: UIColor) {
+        self.blackButton.alpha = color == UIColor.black ? 1 : 0.2
+        self.blueButton.alpha = color == UIColor.blue ? 1 : 0.2
+        self.orangeButton.alpha = color == UIColor.orange ? 1 : 0.2
+    }
+}
